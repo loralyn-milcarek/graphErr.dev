b3a8510c71afc017952493f23546afe964b7c203
loralyn-milcarek <loralyn.milcarek@gmail.com>
2022-06-14 11:49:54 -0400

    initial layout and styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,84 @@
 import Head from 'next/head'
-import { Container, Row, Card, Button } from 'react-bootstrap'
+import { Container, Row, Card, Button, Nav } from 'react-bootstrap'
+
+
+
 
 export default function Home() {
   return (
-    <Container className="md-container">
+    <Container className="md-container" style={{backgroundColor: "#6d6e71"}}>
       <Head>
         <title>ReactJS with react-bootstrap</title>
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
+      
+      <Nav className="justify-content-end" style={{color: "#18341a"}} variant="pills" activeKey="/install">
+        <Nav.Item>
+          <Nav.Link href="/home" style={{color: "#18341a"}}>Links</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+        <Nav.Link eventKey="link-1" style={{color: "#18341a"}}>Team</Nav.Link>
+          </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2" style={{color: "#18341a"}}>Demo</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/install" style={{backgroundColor: "#59af95"}}>Install graphErr</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+
       <Container>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 style={{color: "#18341a"}}>
+          graphErr
         </h1>
-        <p>
-          Get started by editing <code>pages/index.js</code>
+        <p style={{color: "#18341a"}}>
+          Descriptive error handling library for GraphQL + Deno
         </p>
         <Container>
-          <Row className="justify-content-md-between">
+        <Container className="grid-gap">
+          <Card body style={{padding: 4 + 'em', marginTop: 4 + 'em', color: 'white', backgroundColor: '#59af95'}}>Here is a paragraph about graphErr. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Card>
+          <Card body style={{padding: 4 + 'em', marginTop: 4 + 'em', color: 'white', backgroundColor: '#59af95'}}>This is the problem. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card>
+          <Card body style={{padding: 4 + 'em', marginTop: 4 + 'em', color: 'white', backgroundColor: '#59af95'}}>Wow, we have the solution.</Card>
+          </Container>
+          {/* <Card>
+          <Card.Header>first one</Card.Header>
+          <Card.Body>
+            <blockquote className="blockquote mb-0">
+              <p>
+                {' '}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                erat a ante.{' '}
+              </p>
+              <footer className="blockquote-footer">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </footer>
+            </blockquote>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Header>other one</Card.Header>
+          <Card.Body>
+            <blockquote className="blockquote mb-0">
+              <p>
+                {' '}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                erat a ante.{' '}
+              </p>
+              <footer className="blockquote-footer">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </footer>
+            </blockquote>
+          </Card.Body>
+        </Card> */}
+
+
+          {/* <Row className="justify-content-md-between">
             <Card className="sml-card">
               <Card.Body>
-                <Card.Title>Documentation</Card.Title>
+                <Card.Title>About</Card.Title>
                 <Card.Text>
-                  Find in-depth information about Next.js features and API.
+                  short paragraph about graphErr
                 </Card.Text>
                 <Button variant="primary" href="https://nextjs.org/docs">
                   More &rarr;
@@ -70,7 +127,7 @@ export default function Home() {
                 </Button>
               </Card.Body>
             </Card>
-          </Row>
+          </Row> */}
         </Container>
       </Container>
 
