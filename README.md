7a29be0bad000aa14a03b52247c01196030afda0
loralyn-milcarek <loralyn.milcarek@gmail.com>
2022-06-15 15:52:26 -0400

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# GraphErr.dev website
+# graphErr.land
+
+### Homepage for [graphErr](https://github.com/oslabs-beta/graphErr)
+
+<div align="left">
+  <img style="width: 70%" src="./public/site.png" alt="graphErr logo">
+</div>
+
+GraphErr provides descriptive GraphQL error handling for Deno/Oak servers.
+
+<hr/>
+
+### View the graphErr repo on [GitHub](https://github.com/oslabs-beta/graphErr) 
+
+### Visit us at [graphErr.land](https://grapherr.land)
+
+### Add graphErr to your GraphQL project in [Deno](https://deno.land/x/grapherr)
+
+### Connect with the graphErr team on [LinkedIn](https://linkedin.com/company/grapherr)
+
+
--- pages/demo.jsx
+++ pages/demo.jsx
@@ -1,31 +1,55 @@
 import Head from 'next/head';
 import { Container, Row, Card, Button, Nav } from 'react-bootstrap';
 import NavBar from '../components/nav.jsx'
+import Image from 'next/image';
 
 export default function Demo() {
   return (
     <Container className="md-container">
       <Head>
         <title>ReactJS with react-bootstrap</title>
-        <link rel="icon" href="/favicon-32x32.png"/>
+        <link rel="icon" href="/graphErrLogo.svg"/>
       </Head>
 
       <NavBar/>
 
-        <div>
-          {/* header? */}
-        </div>
+
         <Container className="grid-gap">
-          <Card className="cards"> Here's is that demo 
+          <Card className="cards"> 
+            <h1>Making a Query </h1> 
+            Here is an example query in the GraphQL playground:
+            <pre><code>{
+            `
+    query {
+      allUsers {
+        username
+      }
+    }
+            `}
+            </code></pre>
+            The response would look like:
+            <Image src="/qr1.png" alt="example query response" width="826" height="484"/>
+
+            {/* <Image src="/qr1.png" alt="example query response" width="300" height="180" /> */}
           </Card>
-          <div>
-            add a screenshot demo stuff 
-          </div>
-          <Card className="cards"> Here is somemore demo stuff
+
+          <Card className="cards"> 
+            <h1>Functionality </h1> 
+            
+            Example #1 - graphErr response when the user receives an error (null response) that GraphQL does not natively provide error messaging for:
+            {/* <Image src="/qr1.png" alt="example query response" width="413" height="242" /> */}
+{/*             
+            Same query without graphErr:
+            <Image src="/qr1.png" alt="example query response" width="300" height="180"/>
+
+            Example #2 - graphErr response for a native GraphQL error:
+            <Image src="/qr1.png" alt="example query response" width="300" height="180" />
+
+            Same query without graphErr:
+            <Image src="/qr1.png" alt="example query response" width="300" height="180" /> */}
+
           </Card>
-          <div>
-            add a screenshot demo stuff 
-          </div>
+  
 
         </Container>
         
