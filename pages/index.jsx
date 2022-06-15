b82522e76aa2b2b597dbb0cd7c5c79db2f9108d2
loralyn-milcarek <loralyn.milcarek@gmail.com>
2022-06-14 21:05:22 -0400

    added team photos and homepage content Co-Authored-By Avi Kerson <avitacos@gmail.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -34,8 +34,8 @@ export default function Home() {
         </div>  */}
         <Container>
           <Row style={{marginTop: 4 + 'em'}}>
-            <Col xs={5}>
-              <Image src="/graphErrLogo.svg" alt="graphErr Logo" width="300" height="150"/>
+            <Col xs={5} style={{paddingBottom: .5 + 'em'}}>
+              <Image src="/graphErrLogo.svg" alt="graphErr Logo" width="300" height="180" />
             </Col>
             <Col xs={6} style={{paddingTop: 2 + 'em', fontSize: 1.5 + 'em'}}>
             Descriptive error handling library for GraphQL + Deno
@@ -50,8 +50,9 @@ export default function Home() {
         </p>
         <Container>
         <Container className="grid-gap">
-          <Card className="cards">Here is a paragraph about graphErr. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Card>
-          <Card className="cards">This is the problem. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card>
+          <Card className="cards">The Problem: GraphQL is a strongly typed, introspective query language that solves the standard REST response issue of over- and under-fetching data. Even as multi-system queries are abstracted away, some of the benefits of the static, explicit architecture of a RESTful API are lost with GraphQL…
+          </Card>
+          <Card className="cards">Solution: There is currently no lightweight, free library to generate readable, actionable error data that enables query debugging when using GraphQL in Deno. graphErr is an un-opinionated, lightweight library that provides descriptive, debuggable error data to developers working with GraphQL in Deno. In addition to expanded and illustrative error responses pulled directly from the formal GraphQL spec sheet, graphErr will programmatically analyze your GraphQL queries, adding a post-processing layer of error handling that reduces the conceptual burden on the developer by providing many of the benefits of RESTful API architecture.</Card>
           <Card className="cards">Wow, we have the solution.</Card>
           </Container>
 
@@ -61,43 +62,43 @@ export default function Home() {
           <Card body style={{padding: 8 + 'em', marginTop: 8 + 'em', color: 'white', backgroundColor: '#59af95'}}>These are the links. Can we jump to them?</Card>
         </Container>
 
-        <Container id="team" style={{}}>
+        <Container id="team">
           <Card className="text-center" style={{marginTop: 8 + 'em', marginBottom: 1 + 'em', color: '18341a', fontSize: 2 + 'em', backgroundColor: '#F5F7F7', border: 'none'}}>
             We are Team GraphErr
           </Card>
-          <Row style={{display: 'flex', justifyContent: 'space-between', marginBottom: 8 + 'em'}}>
-            
-          
-            <Card style={{width: 125 + 'px', display: 'flex', justifyContent: 'space-around', border: 'none', backgroundColor: '#F5F7F7'}}>
-              <Card.Img variant="top" src="/lm.png" style={{borderRadius: 50 + 'px', width: 100 + 'px', height: 100 + 'px'}}/>
+
+          <Row style={{display: 'flex', justifyContent: 'space-around', marginBottom: 8 + 'em'}}>
+              <Card className="text-center" style={{backgroundColor: '#F5F7F7', border: 'none'}}>
+              <Card.Img variant="top" src="/ak.png" style={{borderRadius: 50 + 'px', width: 100 + 'px', height: 100 + 'px'}}/>
               <Card.Body>
                 <Card.Link href="https://github.com/avitacos">Avi</Card.Link>
               </Card.Body>
-            </Card>
-            <Card style={{width: 125 + 'px', display: 'flex', justifyContent: 'space-around', border: 'none', backgroundColor: '#F5F7F7'}}>
+              </Card>
+          
+            <Card className="text-center" style={{backgroundColor: '#F5F7F7', border: 'none'}}>
               <Card.Img variant="top" src="/lm.png" style={{borderRadius: 50 + 'px', width: 100 + 'px', height: 100 + 'px'}}/>
               <Card.Body>
                 <Card.Link href="https://github.com/loralyn-milcarek">Loralyn</Card.Link>
               </Card.Body>
             </Card>
-            <Card style={{width: 125 + 'px', display: 'flex', justifyContent: 'space-around', border: 'none', backgroundColor: '#F5F7F7'}}>
-              <Card.Img variant="top" src="/lm.png" style={{borderRadius: 50 + 'px', width: 100 + 'px', height: 100 + 'px'}}/>
+            <Card className="text-center" style={{backgroundColor: '#F5F7F7', border: 'none'}}>
+              <Card.Img variant="top" src="/mc.png" style={{borderRadius: 50 + 'px', width: 100 + 'px', height: 100 + 'px'}}/>
               <Card.Body>
                 <Card.Link href="https://github.com/maxwellcook">Max</Card.Link>
               </Card.Body>
             </Card>
-            <Card style={{width: 125 + 'px', display: 'flex', justifyContent: 'space-around', border: 'none', backgroundColor: '#F5F7F7'}}>
-              <Card.Img variant="top" src="/lm.png" style={{borderRadius: 50 + 'px', width: 100 + 'px', height: 100 + 'px'}}/>
+            <Card className="text-center" style={{backgroundColor: '#F5F7F7', border: 'none'}}>
+              <Card.Img variant="top" src="/ts.png" style={{borderRadius: 50 + 'px', width: 100 + 'px', height: 100 + 'px'}}/>
               <Card.Body>
                 <Card.Link href="https://github.com/thomasseo1">Thomas</Card.Link>
               </Card.Body>
             </Card>
-            <Card style={{width: 125 + 'px', display: 'flex', border: 'none', backgroundColor: '#F5F7F7'}}>
+            <Card className="text-center" style={{backgroundColor: '#F5F7F7', border: 'none'}}>
               <Card.Img variant="top" src="/cs.png" style={{borderRadius: 50 + 'px', width: 100 + 'px', height: 100 + 'px'}}/>
               <Card.Body>
                 <Card.Link href="https://github.com/claysawyer">Clay</Card.Link>
               </Card.Body>
-              </Card>
+            </Card>
 
 
           </Row>
